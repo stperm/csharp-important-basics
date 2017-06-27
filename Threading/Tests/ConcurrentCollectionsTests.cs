d487f1a9e3b08996627ae11346e66d0d1fa2c8dd
stperm <realstperm@gmail.com>
2017-06-28 01:06:50 +0200

    Added concurrent collections examples

--- Threading/Tests/ConcurrentCollectionsTests.cs
+++ Threading/Tests/ConcurrentCollectionsTests.cs
@@ -15,56 +15,64 @@ namespace Tests
     public class ConcurrentCollectionsTests
     {
         [Test]
-        [Ignore("TODO")]
-        public void ToDo()
+        public void Using_ConcurrentDictionary()
         {
-            // CONCURRENT DICTIONARY -----------------------------------------------
-
             var dict = new ConcurrentDictionary<int, string>();
 
-            dict.TryAdd(4, "four");
-            dict.TryAdd(5, "five");
-            dict.TryAdd(5, "five 2");
-            dict.TryAdd(6, "six");
+            bool firstFiveSuccess = false, secondFiveSuccess = false;
+
+            Parallel.Invoke(
+                () => dict.TryAdd(4, "four"),
+                () => firstFiveSuccess = dict.TryAdd(5, "five"),
+                () => secondFiveSuccess = dict.TryAdd(5, "five 2"),
+                () => dict.TryAdd(6, "six")
+            );
+
+            Assert.AreNotEqual(firstFiveSuccess, secondFiveSuccess);
+
+            string result;
+            Assert.IsTrue(dict.TryRemove(6, out result));
+            Assert.AreEqual("six", result);
 
             Parallel.Invoke(
                 () => dict.AddOrUpdate(4,
                     i => "four",
-                    (i, old) => old == "four" ? "ha-ha" :
-                                old == "updated four" ? "updated twice four" : "oops"),
-                () => dict.TryUpdate(4, "updated four", "four")
+                    (i, old) => old == "four" ? "ha-ha" : // we are executed before the second Action
+                                old == "updated four" ? "updated twice four" : "oops"), 
+                () => dict.TryUpdate(4, "updated four", "four") // we are executed before the first action
             );
+                        
+            Assert.IsTrue(dict.TryGetValue(4, out result));
+            Assert.IsTrue((new[] { "updated four", "updated twice four" }).Contains(result));            
+        }
 
-            PrintCollection(dict);
-
-            // BLOCKING COLLECTION ----------------------------------------------------------
-
+        [Test]
+        public void Using_BlockingCollection()
+        {
             // bounded capacity, blocking Add until the item is taken,
             // uses concurrentQueue by default, but may be customized 
-            var blocking = new BlockingCollection<int>(new ConcurrentStack<int>(new[] { 1, 2, 3 }), 3);
-
-            //blocking.TryAdd(1);
-            //blocking.TryAdd(2);
-            //blocking.TryAdd(3);
+            var blocking = new BlockingCollection<int>(new ConcurrentStack<int>(new[] { 1, 2, 3 }), 3);            
 
             Task.Run(() =>
             {
-                System.Threading.Thread.Sleep(3000);
+                System.Threading.Thread.Sleep(150);
                 int taken;
                 var isTaken = blocking.TryTake(out taken);
-                Console.WriteLine("{0}taken - {1}", isTaken ? "" : "not ", taken);
+                Assert.IsTrue(isTaken);
+                Assert.AreEqual(3, taken);
             });
 
-            Console.WriteLine("trying...");
-            if (!blocking.TryAdd(4))
-                Console.WriteLine("failed");
-            blocking.Add(5);
-            Console.WriteLine("done");
+            Assert.IsFalse(blocking.TryAdd(4)); // this call is not blocked, but might not succeed
+            blocking.Add(5); // this call is blocked until there is a free space in the collection
+            var result = blocking.Take();
+            Assert.AreEqual(5, result);
+        }
 
-            PrintCollection(blocking);
-
-            // CONCURRENT BAG ----------------------------------------------------
-
+        [Test]
+        public void Using_ConcurrentBag()
+        {
+            // one important feature of ConcurrentBag is that
+            // values are returned first to the thread which added them
             var bag = new ConcurrentBag<int>();
 
             var addTask = Task.Factory.StartNew(() =>
@@ -74,57 +82,88 @@ namespace Tests
                 bag.Add(6);
             });
 
-            addTask.Wait();
-
             bag.Add(1);
-            bag.Add(1);
+            bag.Add(1); // this value is also stored despite the fact the same value is already in
             bag.Add(2);
+
+            addTask.Wait();
 
             int result;
 
-            if (bag.TryPeek(out result))
-                Console.WriteLine("bag peek: {0}", result);
+            Assert.IsTrue(bag.TryPeek(out result));
+
+            var currentThreadValues = new[] { 1, 2 };
+
+            Assert.IsTrue(currentThreadValues.Contains(result));
+
+            var anotherThreadValues = new[] { 4, 5, 6 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsTrue(bag.TryTake(out result));
+                Assert.IsTrue(currentThreadValues.Contains(result));
+            }
 
             while (bag.TryTake(out result))
             {
-                Console.WriteLine("bag: {0}", result);
+                Assert.IsTrue(anotherThreadValues.Contains(result));
             }
+        }
 
-            // CONCURRENT STACK ---------------------------------------------------------- 
-
+        [Test]
+        public void Using_ConcurrentStack()
+        {            
             var stack = new ConcurrentStack<int>();
 
             stack.Push(2);
             stack.Push(4);
             stack.Push(6);
 
-            if (stack.TryPeek(out result))
-                Console.WriteLine("stack peek: {0}", result);
+            int result;
+
+            Assert.IsTrue(stack.TryPeek(out result));
+            Assert.AreEqual(6, result);
+
+            var sb = new StringBuilder(3);
 
             while (stack.TryPop(out result))
             {
-                Console.WriteLine("stack: {0}", result);
+                sb.Append(result);
             }
 
-            // CONCURRENT QUEUE ----------------------------------------------------------
+            Assert.AreEqual("642", sb.ToString()); // usual stack behavior
+        }
 
+        [Test]
+        public void Using_ConcurrentQueue()
+        {
             var queue = new ConcurrentQueue<int>();
 
             queue.Enqueue(1);
             queue.Enqueue(2);
             queue.Enqueue(3);
-            queue.Enqueue(4);
 
-            if (queue.TryPeek(out result))
-                Console.WriteLine("queue peek: {0}", result);
+            int result;
+
+            Assert.IsTrue(queue.TryPeek(out result));
+            Assert.AreEqual(1, result);
+
+            var sb = new StringBuilder(3);
 
             while (queue.TryDequeue(out result))
             {
-                Console.WriteLine("queue: {0}", result);
+                sb.Append(result);
             }
 
+            Assert.AreEqual("123", sb.ToString()); // usual queue behavior
+        }
+
+        [Test]
+        
+        public void Using_IProducerConsumerCollection()
+        {              
             // IProducerConsumerCollection<T> - abstraction over producer-consumer collections
-            // queue, stack and bag implement this interface
+            // CocncurrentQueue, ConcurrentStack and ConcurrentBag implement this interface
 
             IProducerConsumerCollection<int> prodConsCollection = new ConcurrentQueue<int>();
 
@@ -132,18 +171,14 @@ namespace Tests
             prodConsCollection.TryAdd(2);
             prodConsCollection.TryAdd(3);
 
+            int result;
+            var sb = new StringBuilder(3);
             while (prodConsCollection.TryTake(out result))
             {
-                Console.WriteLine("prodCons {0}", result);
+                sb.Append(result);
             }
-        }
 
-        private static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
-            {
-                Console.WriteLine("{0}", item);
-            }
-        }
+            Assert.AreEqual("123", sb.ToString()); // obvious behavior of the real collection
+        }        
     }
 }
---
+++ Threading/Tests/SynchronizationPrimitivesTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingTests
+{
+    [TestFixture]
+    [Ignore("TODO")]
+    public class SynchronizationPrimitivesTests
+    {
+        [Test]
+        public void Using_Monitor()
+        {
+
+        }
+
+        [Test]
+        public void UsingMutex()
+        { 
+
+        }
+
+        [Test]
+        public void Using_Semaphore()
+        {
+
+        }
+
+        [Test]
+        public void Using_SemaphoreSlim()
+        {
+
+        }
+
+        [Test]
+        public void Using_SpinWait()
+        {
+
+        }
+
+        [Test]
+        public void Using_ReaderWriterLockSlim()
+        {
+
+        }
+        
+        [Test]
+        public void Using_AutoResetEvent()
+        {
+
+        }
+
+        [Test]
+        public void Using_ManualResetEvent()
+        {
+
+        }
+
+        [Test]
+        public void Using_Interlocked()
+        {
+
+        }
+    }
+}
